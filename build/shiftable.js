f1577318e4cde92126360d1a9edc442255d50870
zim <zimtsui@gmail.com>
2022-06-05 17:59:05 +0800

    comment

--- build/shiftable.js
+++ build/shiftable.js
@@ -1,4 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// TODO why not extending Iterable ot Iterator
 //# sourceMappingURL=shiftable.js.map
--- src/shiftable.ts
+++ src/shiftable.ts
@@ -2,5 +2,3 @@ export interface Shiftable<T> {
 	i(index: 0): T;
 	shift(): T;
 }
-
-// TODO why not extending Iterable ot Iterator
